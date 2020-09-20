35bd4aa6aacbce2fe1b2782115b0204c6ed1d82d
Jason <youchia1991@gmail.com>
2020-09-19 23:23:09 -0400

    completed generateAnswers,populateArray, preLoad functions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const potterApp = {};
+
 potterApp.scenarios = [
     {
         question: `It's the day of the first task, and you're as ready as you will ever be. Waiting in the tent, listening to the crowd's screams and the roars of the dragon, horrible pictures start forming in your mind. You shake your head in frustration and attempt to focus on the one spell that you practiced since you received Professor Moody's advice.
@@ -8,7 +9,6 @@ potterApp.scenarios = [
         "3....2....1....Begin!"
         The whistle is blown. With every fiber of being, you raise your wand and bellow the following incantation:`,
         answer: ["accio"],
-        
     },
     {
         question: "",
@@ -31,81 +31,41 @@ potterApp.scenarios = [
 // potterApp.command = "";
 potterApp.key = '$2a$10$giwowS/BIXhApClXp86ptu2X/k4gC80zFZM8sxnDRlUSReHJnH1JO';
 potterApp.url = 'https://www.potterapi.com/v1/';
-potterApp.spells = [];
-potterApp.characters = [];
 potterApp.fourOptions = [];
-potterApp.name = {firstName: "", lastName: ""};
-// potterApp.charmsArray = potterApp.spells.filter(spell => spell.type == "Charm");
-// potterApp.profArray = potterApp.characters.filter(char => char.role == "Professor");
 
-potterApp.generateAnswers = function() {
-    for (let i = 0; i < 4; i++) {
-        potterApp.populateArray(potterApp.fourOptions);
+potterApp.generateAnswers = function(array, point) {
+    potterApp.fourOptions.splice(0,potterApp.fourOptions.length)
+    while (potterApp.fourOptions.length < 4)
+    {
+        potterApp.populateArray(potterApp.fourOptions, array, point);
     }
 }
 
-potterApp.populateArray = function(array) {
-    // we need to work on this
-    const number = Math.floor(Math.random() * charmsArray.length);
-    const length = array.length;
-    for (let i = 0; i < length; i++) {
-        if (number == array[i] ) {
-            quizApp.populateArray(array);
-        }
+potterApp.populateArray = function(targetArray, fromArray, point) {
+    const number = Math.floor(Math.random() * fromArray.length);
+    if (targetArray.includes(fromArray[number][point]) == false)
+    {
+        targetArray.push(fromArray[number][point]);
     }
-    array.push(number);
 }
 
+potterApp.getCharacters = $.ajax({
+    url: `${potterApp.url}characters`,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+        key: potterApp.key,
+    }
+});
 
-
-potterApp.getSpells = function () {
-    $.ajax({
-        url: `${potterApp.url}spells`,
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            key: potterApp.key,
-            format: 'json',
-        }
-    }).then((res) => {
-        res.forEach((element) => {
-            let spellObject =  {
-                spell: element.spell,
-                type: element.type,
-                effect: element.effect,
-            }
-            potterApp.spells.push(spellObject)
-        })
-    })
-}
-
-potterApp.getCharacters = function () {
-    $.ajax({
-        url: `${potterApp.url}characters`,
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            key: potterApp.key,
-        }
-    }).then((res) => {
-        res.forEach((element) => {
-            let charObject =  {
-                id: element._id,
-                name: element.name,
-                role: element.role,
-                house: element.house,
-                ministryOfMagic: element.ministryOfMagic,
-                orderOfThePhoenix: element.orderOfThePhoenix,
-                dumbledoresArmy: element.dumbledoresArmy,
-                deathEater: element.deathEater,
-                bloodStatus: element.bloodStatus,
-                species: element.species,
-            }
-            potterApp.characters.push(charObject)
-        })
-    })
-}
-
+potterApp.getSpells = $.ajax({
+    url: `${potterApp.url}spells`,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+        key: potterApp.key,
+    }
+});
 
 potterApp.sortHat = $.ajax({
     url: `${potterApp.url}sortingHat`,
@@ -113,20 +73,30 @@ potterApp.sortHat = $.ajax({
     dataType: 'json',
 });
 
+potterApp.preLoad = () => {
+    potterApp.getCharacters.then((res) => {
+        potterApp.profArray = res.filter(char => char.role != undefined && char.role.toLowerCase().includes("professor") && (char.deathEater == false || char.orderOfThePhoenix == true));
+    });
+    potterApp.getSpells.then((res) => {
+        potterApp.charmsArray = res.filter(spell => spell.type.toLowerCase() == "charm");
+        potterApp.harmSpellsArray = res.filter(spell => spell.type.toLowerCase() == "curse" || spell.type.toLowerCase() == "hex");
+    });
+}
+
 potterApp.getSorted = () => {
-    $( "#sort" ).submit(function( event ) {
+    $( "#sort" ).submit(function(event) {
         event.preventDefault(event);
-        if ($("#firstName").val() == '' || $("#lastName").val() == "" ) {
+        if ($("#firstName").val() == "" || $("#lastName").val() == "" ) {
             alert('Please fill in your full name!');
         } else {
             potterApp.sortHat.then((res) => {
                 $(`.${res.toLowerCase()}`).css("display", "flex");
                 $(".startAdventure").css("display", "block");
                 $(".studentHouse").text(res);
-                potterApp.name.firstName = $("#firstName").val();
-                potterApp.name.lastName = $("#lastName").val();
-                console.log(potterApp.name);
-                
+                potterApp.name = {
+                    firstName: $("#firstName").val(),
+                    lastName: $("#lastName").val()
+                } 
             })
             // used to disable resubmitting form, lock input text
             $("#sort input[type='submit'], #sort input[type='name']").attr("disabled", true);
@@ -146,23 +116,21 @@ potterApp.begin = () => {
     $(".beginScenario button").click((event) => {
         event.preventDefault(event);
         $(".preStoryPage, .beginScenario").css("display", "none");
+        potterApp.generateAnswers(potterApp.charmsArray,"spell")
     })
+    
 }
 
-
-
-// potterApp.getSpells();
-// potterApp.getCharacters();
 potterApp.init = function() {
+    potterApp.preLoad();
     potterApp.getSorted();
     potterApp.startAdventure();
     potterApp.begin();
 }
+
 $(document).ready(function(){
     potterApp.init();
 });
-
-
 
 // potterApp.getHouses = function () {
 //     $.ajax({
